b211283ce5a476061406a150b896982a6fd285ec
Sammy W <swucode@gmail.com>
2017-10-14 02:11:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+let feet
+
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  }
+  else if (feet > 2000 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500 ) {
+    return 'No can do.'
+  }
+}
+
+let city
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+
+let tip
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+    break;
+    default:
+      return 'Bye.';
+  }
+}
